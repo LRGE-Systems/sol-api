d9e9e52f52df8813b3a313c44aeeb4870a3151f9
Galba Arueira <arueira95@gmail.com>
2022-12-01 17:14:25 -0300

    refactor: migration to mariadb

--- db/migrate/20180721142413_create_addresses.rb
+++ db/migrate/20180721142413_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.references :addressable, polymorphic: true, index: true
+      t.references :addressable, polymorphic: true
       t.decimal :latitude, precision: 11, scale: 8
       t.decimal :longitude, precision: 11, scale: 8
       t.string :address
