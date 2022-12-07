928a71dea0d3e7ef41c7d758fa343c853f30d629
Galba Arueira <arueira95@gmail.com>
2022-12-06 22:49:14 -0300

    refactor: changing specific json access pg

--- app/models/concerns/data_attributable.rb
+++ app/models/concerns/data_attributable.rb
@@ -5,10 +5,10 @@ module DataAttributable
     def data_attr(*attrs)
       attrs.each do |attr|
         attr_as_string = attr.to_s
-        puts "AAAAAAAAAAAAAAAAAAAAAAAAAA"
-        puts "SEEEE THIS"
-        puts attr 
-        puts attr_as_string
+        logger.info "AAAAAAAAAAAAAAAAAAAAAAAAAA"
+        logger.info "SEEEE THIS"
+        logger.info attr 
+        logger.info attr_as_string
         if attr_as_string != "[]"
           define_method(attr) { data[attr_as_string] }
           define_method("#{attr}=") { |val| data[attr_as_string] = val }
